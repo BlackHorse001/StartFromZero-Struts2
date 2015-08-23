d3c19ee61994a9deecbeec6febf0ce9cacb7d061
Impler <704090860@qq.com>
2015-08-23 16:21:17 +0800

    configure action

---
+++ Struts2/src/main/java/com/study/Struts2/action/BasicAction.java
@@ -0,0 +1,13 @@
+package com.study.Struts2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BasicAction extends ActionSupport{
+
+	private static final long serialVersionUID = -698833595216648246L;
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+}
