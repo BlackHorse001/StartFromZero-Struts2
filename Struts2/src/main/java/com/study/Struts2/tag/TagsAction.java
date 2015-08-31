e858e473c8514c0c27e25e56c940bd00beb60d7c
Impler <704090860@qq.com>
2015-08-31 22:17:43 +0800

    struts tags example

--- Struts2/src/main/java/com/study/Struts2/tag/TagsAction.java
+++ Struts2/src/main/java/com/study/Struts2/tag/TagsAction.java
@@ -5,11 +5,17 @@ import com.opensymphony.xwork2.ActionSupport;
 public class TagsAction extends ActionSupport{
 
 	private static final long serialVersionUID = -6423262867869142863L;
-
+	
+	private int flag = -1;
 	@Override
 	public String execute() throws Exception {
 		return SUCCESS;
 	}
 	
-	
+	public int getFlag() {
+		return flag;
+	}
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
 }
