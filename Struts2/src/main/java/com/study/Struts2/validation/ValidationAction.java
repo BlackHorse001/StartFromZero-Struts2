346a8d866f91d8aeebd1f1773ca5db968cc9485b
Impler <704090860@qq.com>
2015-08-31 22:31:32 +0800

    struts validation

---
+++ Struts2/src/main/java/com/study/Struts2/validation/ValidationAction.java
@@ -0,0 +1,9 @@
+package com.study.Struts2.validation;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ValidationAction extends ActionSupport {
+
+	private static final long serialVersionUID = -6704175133780795746L;
+
+}
